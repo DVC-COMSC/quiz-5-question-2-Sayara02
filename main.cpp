013d95eaec2da32b901123e71f7eaa11192ab999
Sayara02 <azizovna02@gmail.com>
2024-03-08 19:45:33 -0800

    2 run

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ int main()
    /* Complete your code */
    if (N <= 0 || N >= 10 || M < 1 || M > 10) {
       cout << "Invalid input! Ensure 0 < N < 10 and 1 <= M <= 10." << endl;
+      return 1;
+   }
+   cout << N << " to the power number: ";
+   int result = 1;
+   for (int i = 0; i <= M; ++i) {
+      cout << result;
       
    }
   
